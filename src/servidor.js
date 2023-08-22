7350d6f952533c9af5c1437b3c5b69168bcfce8c
Paul Fontes <paulfontes1@gmail.com>
2023-08-22 20:38:05 -0300

    Iniciando desafio.

--- src/servidor.js
+++ src/servidor.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+const choroLivre = 'chora hoje, amanhã a gente tenta';
+console.log(choroLivre);
+
 app.use(express.json());
 
 module.exports = app;
