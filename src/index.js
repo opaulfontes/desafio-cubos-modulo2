51d1a5fb134fe9a9240852fa830a781014c421d9
Paul Fontes <paulfontes1@gmail.com>
2023-08-30 11:14:17 -0300

    rotas cadastradas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 const app = require('./servidor');
 
+app.get('/contas?senha_banco=123');
+app.post('/contas');
+app.put('/contas/:numeroConta/usuario');
+app.delete('/contas/:numeroConta');
+app.post('/transacoes/depositar');
+app.post('/transacoes/sacar');
+app.post('/transacoes/transferir');
+app.get('/contas/saldo?numero_conta=123&senha=123');
+app.get('/contas/extrato?numero_conta=123&senha=123');
+
 app.listen(3000);
--- src/servidor.js
+++ src/servidor.js
@@ -2,9 +2,6 @@ const express = require('express');
 
 const app = express();
 
-const choroLivre = 'chora hoje, amanhã a gente tenta';
-console.log(choroLivre);
-
 app.use(express.json());
 
 module.exports = app;
