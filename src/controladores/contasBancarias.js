a0d3b5e6e99155888439e9b8ad1a3333d6ad1b2e
Paul Fontes <paulfontes1@gmail.com>
2023-08-31 20:58:01 -0300

    Pasta de controladores criada e configurada

---
+++ src/controladores/contasBancarias.js
@@ -0,0 +1,31 @@
+const contasBancarias = require('../bancodedados');
+
+const listaContas = (req, res) => {}
+
+const criaConta = (req, res) => {}
+
+const atualizaDadosConta = (req, res) => {}
+
+const excluirConta = (req, res) => {}
+
+const depositoConta = (req, res) => {}
+
+const saqueConta = (req, res) => {}
+
+const transferenciaEntreContas = (req, res) => {}
+
+const consultarSaldoConta = (req, res) => {}
+
+const emitirExtratoBancario = (req, res) => {}
+
+module.exports = {
+    listaContas,
+    criaConta,
+    atualizaDadosConta,
+    excluirConta,
+    depositoConta,
+    saqueConta,
+    transferenciaEntreContas,
+    consultarSaldoConta,
+    emitirExtratoBancario,
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+const { contas } = require('./bancodedados');
 const app = require('./servidor');
+const { 
+    listaContas, 
+    criaConta, 
+    atualizaDadosConta, 
+    excluirConta, 
+    depositoConta,
+    saqueConta, 
+    transferenciaEntreContas, 
+    consultarSaldoConta, 
+    emitirExtratoBancario
+} = require('./controladores/contasBancarias')
 
-app.get('/contas?senha_banco=123');
-app.post('/contas');
-app.put('/contas/:numeroConta/usuario');
-app.delete('/contas/:numeroConta');
-app.post('/transacoes/depositar');
-app.post('/transacoes/sacar');
-app.post('/transacoes/transferir');
-app.get('/contas/saldo?numero_conta=123&senha=123');
-app.get('/contas/extrato?numero_conta=123&senha=123');
+app.get('/contas?senha_banco=123', listaContas);
+app.post('/contas', criaConta);
+app.put('/contas/:numeroConta/usuario', atualizaDadosConta);
+app.delete('/contas/:numeroConta', excluirConta);
+app.post('/transacoes/depositar', depositoConta);
+app.post('/transacoes/sacar', saqueConta);
+app.post('/transacoes/transferir', transferenciaEntreContas);
+app.get('/contas/saldo?numero_conta=123&senha=123', consultarSaldoConta);
+app.get('/contas/extrato?numero_conta=123&senha=123', emitirExtratoBancario);
 
 app.listen(3000);
