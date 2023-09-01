8bd9cfdfcbb830ae0f414e5f871603f27d345cd4
Paul Fontes <paulfontes1@gmail.com>
2023-09-01 01:43:26 -0300

    Novas alterações dos controladores

--- src/index.js
+++ src/index.js
@@ -12,14 +12,26 @@ const {
     emitirExtratoBancario
 } = require('./controladores/contasBancarias')
 
-app.get('/contas?senha_banco=123', listaContas);
+const verificarNumeroConta = (req, res, next) => {
+    return res.send('Número da conta obrigatório');
+    next();
+}
+const verificarSenha = (req, res, next) => {
+    return res.send('Senha inválida');
+    next();
+}
+
+app.use(verificarNumeroConta);
+app.use(verificarSenha);
+
+app.get('/contas?senha_banco=123', verificarSenha, listaContas);
 app.post('/contas', criaConta);
-app.put('/contas/:numeroConta/usuario', atualizaDadosConta);
-app.delete('/contas/:numeroConta', excluirConta);
+app.put('/contas/:numeroConta/usuario', verificarNumeroConta, atualizaDadosConta);
+app.delete('/contas/:numeroConta', verificarNumeroConta, excluirConta);
 app.post('/transacoes/depositar', depositoConta);
 app.post('/transacoes/sacar', saqueConta);
 app.post('/transacoes/transferir', transferenciaEntreContas);
-app.get('/contas/saldo?numero_conta=123&senha=123', consultarSaldoConta);
-app.get('/contas/extrato?numero_conta=123&senha=123', emitirExtratoBancario);
+app.get('/contas/saldo?numero_conta=123&senha=123', verificarNumeroConta, verificarSenha, consultarSaldoConta);
+app.get('/contas/extrato?numero_conta=123&senha=123', verificarNumeroConta, verificarSenha, emitirExtratoBancario);
 
 app.listen(3000);
